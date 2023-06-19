4afd02231a480d66b38b21bc2992f71269e90060
hamza33797 <sevensol916@gmail.com>
2023-06-19 05:35:22 -0700

    index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser=require('body-parser');
 const authRoutes = require("./Api/routers/auth");
 const messageRoutes = require("./Api/routers/messages");
 const mongoose = require("mongoose");
-const socket = require("socket.io");
+// const socket = require("socket.io");
 require("dotenv").config();
 const cors=require('cors')
 const user_verification=require('./Api/routers/user_verification');
@@ -28,27 +28,27 @@ app.use("/api/messages", messageRoutes);
 app.use('/userverification',user_verification);
 const port= process.env.port||4330;
 const server =require('./server');
-const io = socket(server, {
-    cors: {
-      origin: "http://localhost:3000",
-      credentials: true,
-    },
-  });
+// const io = socket(server, {
+//     cors: {
+//       origin: "http://localhost:3000",
+//       credentials: true,
+//     },
+//   });
   
-  global.onlineUsers = new Map();
-  io.on("connection", (socket) => {
-    global.chatSocket = socket;
-    socket.on("add-user", (userId) => {
-      onlineUsers.set(userId, socket.id);
-    });
+  // global.onlineUsers = new Map();
+  // io.on("connection", (socket) => {
+  //   global.chatSocket = socket;
+  //   socket.on("add-user", (userId) => {
+  //     onlineUsers.set(userId, socket.id);
+  //   });
   
-    socket.on("send-msg", (data) => {
-      const sendUserSocket = onlineUsers.get(data.to);
-      if (sendUserSocket) {
-        socket.to(sendUserSocket).emit("msg-recieve", data.msg);
-      }
-    });
-  });
+  //   socket.on("send-msg", (data) => {
+  //     const sendUserSocket = onlineUsers.get(data.to);
+  //     if (sendUserSocket) {
+  //       socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+  //     }
+  //   });
+  // });
   
 
 
