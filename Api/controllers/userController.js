26c0de415732c4d7dfbebdd637729dae5fa9bd71
hamza33797 <sevensol916@gmail.com>
2023-06-18 23:31:17 -0700

    userController.js

--- Api/controllers/userController.js
+++ Api/controllers/userController.js
@@ -6,7 +6,7 @@ module.exports.login = async (req, res, next) => {
     const { username, password } = req.body;
     const user = await User.findOne({ username });
     if (!user)
-      return res.json({ msg: "Incorrectionsss Username or Password", status: false });
+      return res.json({ msg: "Incorrections Username or Password", status: false });
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid)
       return res.json({ msg: "Incorrect Username or Password", status: false });
